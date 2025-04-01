9eba9ebea2cbc9b79f05708f373fd0dea0e880cc
DarkEXCALIBUR-AI <guoyingjian96@gmail.com>
2025-04-01 16:12:09 +0800

    Add files via upload

--- agent demo/tools/zw3d_command_tool.py
+++ agent demo/tools/zw3d_command_tool.py
@@ -1,3 +1,5 @@
+from sympy.strategies.core import switch
+
 from .tool_base import Tool
 import subprocess
 import json
@@ -63,3 +65,203 @@ class ZW3DCommandTool(Tool):
             "return code": result.returncode
         }
 
+
+class ZW3DCommandOpen(Tool):
+    """
+    Tool for running ZW3D command.
+    """
+    @property
+    def name(self) -> str:
+        return "zw3d_open"
+
+    @property
+    def description(self) -> str:
+        return "Execute ZW3D open command, open a specific file in ZW3D"
+
+    @property
+    def input_schema(self) -> Dict[str, Any]:
+        return {
+            "type": "object",
+            "properties": {
+                "filePath": {
+                    "type": "string",
+                    "description": "file path to open",
+                },
+            },
+            "required": ["filePath"]
+        }
+
+    def run(self, input: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Execute a ZW3D remote command.
+
+        Args:
+            input: Dictionary containing:
+                filePath: file path for the command to open in JSON format
+
+        Returns:
+            Dictionary containing command output, error message, and return code
+        """
+        params = input.get('filePath')
+
+        json_str = {
+            "filePath": f"{params}"
+        }
+
+        json_str = json.dumps(json_str)
+
+        cmd = [
+            'zw3dremote',
+            '-r', 'local',
+            f'cmd=~FILEOPEN({json_str})'
+        ]
+
+        result = subprocess.run(cmd, capture_output=True, text=True)
+
+        return {
+            "stdout": result.stdout.strip(),
+            "stderr": result.stderr.strip(),
+            "return code": result.returncode
+        }
+
+
+class ZW3DCommandExpPDF(Tool):
+    """
+    Tool for running ZW3D command.
+    """
+    @property
+    def name(self) -> str:
+        return "zw3d_expPdf"
+
+    @property
+    def description(self) -> str:
+        return "Execute ZW3D export PDF command, export the active file into a PDF file"
+
+    @property
+    def input_schema(self) -> Dict[str, Any]:
+        return {
+            "type": "object",
+            "properties": {
+                "path": {
+                    "type": "string",
+                    "description": "file path for export",
+                },
+                "pdfType": {
+                    "type": "string",
+                    "description": "export type",
+                    "enum": ["RASTER", "VECTOR", "OBJECT"],
+                    "default": "VECTOR"
+                },
+            },
+            "required": ["path", "pdfType"]
+        }
+
+    def run(self, input: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Execute a ZW3D remote command.
+
+        Args:
+            input: Dictionary containing:
+
+        Returns:
+            Dictionary containing command output, error message, and return code
+        """
+        path = input.get('path')
+        pdfType = input.get('pdfType')
+        if pdfType == "RASTER":
+            pdfType = 0
+        elif pdfType == "VECTOR":
+            pdfType = 1
+        else:
+            pdfType = 2
+
+        json_str = {
+            "path": f"{path}",
+            "pdfType": pdfType
+        }
+
+        json_str = json.dumps(json_str)
+
+        cmd = [
+            'zw3dremote',
+            '-r', 'local',
+            f'cmd=~EXPPDF({json_str})'
+        ]
+
+        result = subprocess.run(cmd, capture_output=True, text=True)
+
+        return {
+            "stdout": result.stdout.strip(),
+            "stderr": result.stderr.strip(),
+            "return code": result.returncode
+        }
+
+class ZW3DCommandCreateStdView(Tool):
+    """
+    Tool for running ZW3D command.
+    """
+    @property
+    def name(self) -> str:
+        return "zw3d_open"
+
+    @property
+    def description(self) -> str:
+        return "Execute ZW3D open command, open a specific file in ZW3D"
+
+    @property
+    def input_schema(self) -> Dict[str, Any]:
+        return {
+            "type": "object",
+            "properties": {
+                "path": {
+                    "type": "string",
+                    "description": "file path to insert",
+                },
+                "rootName":{
+                    "type": "string",
+                    "description": "root name"
+                },
+                "type":{
+                    "type": "int",
+                    "description": "standard view type"
+                }
+            },
+            "required": ["path", "rootName", "type"]
+        }
+
+    def run(self, input: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Execute a ZW3D remote command.
+
+        Args:
+            input: Dictionary containing:
+                filePath: file path for the command to open in JSON format
+
+        Returns:
+            Dictionary containing command output, error message, and return code
+        """
+        path = input.get('path')
+        rootName = input.get('rootName')
+        viewType = input.get('type')
+
+        json_str = {
+            "path": f"{path}",
+            "rootName": f"{rootName}",
+            "type": viewType,
+        }
+
+        json_str = json.dumps(json_str)
+
+        cmd = [
+            'zw3dremote',
+            '-r', 'local',
+            f'cmd=~STDVU({json_str})'
+        ]
+
+        result = subprocess.run(cmd, capture_output=True, text=True)
+
+        return {
+            "stdout": result.stdout.strip(),
+            "stderr": result.stderr.strip(),
+            "return code": result.returncode
+        }
